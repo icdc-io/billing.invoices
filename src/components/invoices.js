ae2a5c34c5e0f746ea316efafb4c9164238b0c46
Yuliya Pakidzka <ypakidzka@ibagroup.eu>
2023-08-21 17:09:29 +0300

    fix : fix search

--- src/components/invoices.js
+++ src/components/invoices.js
@@ -53,6 +53,9 @@ const Invoices = ({ t, isPayEnable, history }) => {
             || item.amount?.toString().toLowerCase().includes(searchString)
             || item.name?.toString().toLowerCase().includes(searchString)
             || item.number?.toString().toLowerCase().includes(searchString)
+            || item.due_date?.toString().toLowerCase().includes(searchString)
+            || item.payment_date?.toString().toLowerCase().includes(searchString)
+            || item.payment_method?.toString().toLowerCase().includes(searchString)
             || status?.toLowerCase().includes(searchString)
     }).sort((a, b) => {
         const date1 = Date.parse(a.payment_date);
